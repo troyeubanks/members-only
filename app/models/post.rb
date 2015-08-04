f312ff56282a1174d1f9439fa575fcbbc7f843d5
Troy Eubanks <TEubanks01@gmail.com>
2015-08-03 23:13:10 -0400

    finished post index

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
 
 	validates :title, presence: true, uniqueness: true
 	validates :body, presence: true
+
+	def get_author_name
+		User.find_by(id: self.user_id).name
+	end
 end
